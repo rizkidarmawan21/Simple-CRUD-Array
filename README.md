46354e1262dc9625ab4a5f008745311f9e28d0de
Rizki Darmawan <rizkidarmawan.0402102@gmail.com>
2022-01-16 05:53:18 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# CRUD with array javascript  Rizki Darmawan
---
+++ script.js
@@ -0,0 +1,33 @@
+var data = [];
+
+
+function tampil() {
+    var tabel = document.getElementById("tabel");
+    tabel.innerHTML = "<tr><th>No</th><th>Item</th><th>Aksi</th></tr>";
+    for (let i = 0; i < data.length; i++) {
+        var btnEdit = "<button class='btn-edit' href='#' onclick='edit(" + i + ")'>Edit</button>";
+        var btnHapus = "<button class='btn-hapus' href='#' onclick='hapus(" + i + ")'>Delete</button>";
+        j = i + 1;
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i] + "</td><td>" + btnEdit + " " + btnHapus + "</tr>";
+    }
+}
+
+function tambah() {
+    var input = document.querySelector("input[name=belanja]");
+    data.push(input.value);
+    tampil();
+    input.value = "";
+}
+
+function edit(id) {
+    var baru = prompt("Edit", data[id]);
+    data[id] = baru;
+    tampil();
+}
+
+function hapus(id) {
+    data.pop(id);
+    tampil();
+}
+
+tampil();
